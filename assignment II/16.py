0b6d8cc8a3b4ce0f2c6c3beaeeafa4599f828657
Aju100 <ajutamang10@outlook.com>
2020-07-06 12:30:42 +0545

    Updated solutions

---
+++ assignment II/16.py
@@ -0,0 +1,16 @@
+class Mario():
+    def __init__(self,name_of_user,sound,direction):
+        self.name_of_user = name_of_user
+        self.sound = sound
+        self.direction = direction
+
+    def move(self):
+        pass
+    def jump(self):
+        pass
+    def stop(self):
+        pass
+    def gameover(self):
+        pass
+    def newlevel(self):
+        pass
